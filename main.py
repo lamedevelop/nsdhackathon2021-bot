7e8de1f79aed505c64f1b08ed71a5b32adf16b94
zoglam <anzoor1998@gmail.com>
2021-05-15 17:40:45 +0300

    init

---
+++ main.py
@@ -0,0 +1,39 @@
+from aiogram import Bot
+from aiogram.utils import executor
+from aiogram.dispatcher import Dispatcher
+
+import os
+import sys
+
+from controllers.logger import Logger
+
+def getToken():
+    if 'BOT_TOKEN' in os.environ:
+        return os.environ['BOT_TOKEN']
+    else:
+        Logger.error('Token does not exist')
+        sys.exit(1)
+
+bot = Bot(token=getToken())
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def chooseUniversity(message):
+    pass
+
+@dp.message_handler(commands=["new_messages"])
+async def chooseUniversity(message):
+    pass
+
+@dp.message_handler(commands=["out_messages"])
+async def chooseUniversity(message):
+    pass
+
+@dp.message_handler(commands=["auth"])
+async def chooseUniversity(message):
+    pass
+
+if __name__ == '__main__':
+    Logger.info("Polling started")
+    executor.start_polling(dp)
+    Logger.info("Polling over")
